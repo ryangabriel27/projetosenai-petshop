a7de53698fe36a6e41a61a3738ccbb6729c209f5
ryangabriel27 <ryanjulio56@gmail.com>
2023-11-09 15:20:38 -0300

    09.11

---
+++ pet-e-repet/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Header from './components/header/header.js';
+
+function App() {
+  return (
+    <><Header></Header>
+    </>
+  );
+}
+
+export default App;
---
+++ pet-e-repet/src/components/home/homePage.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+import './home.css';
+
+export default function HomePage(){
+
+}
