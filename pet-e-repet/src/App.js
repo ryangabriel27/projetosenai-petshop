1ad00a033bc725faefb16cd01e02c697e5215a01
ryangabriel27 <ryanjulio56@gmail.com>
2023-11-17 17:00:41 -0300

    17.11 17h

--- pet-e-repet/src/App.js
+++ pet-e-repet/src/App.js
@@ -2,13 +2,29 @@ import React from 'react';
 import './App.css';
 import Header from './components/header/header.js';
 import HomePage from './components/home/homePage.js';
+import Login from './pages/login.js';
+import Header2 from './components/header2/header2.js';
+import { Routes, Route } from 'react-router-dom';
+
 
 function App() {
-  return (
-    <><Header></Header>
-      <HomePage></HomePage>
-    </>
-  );
+  // let header;
+  // switch (window.location.pathname) {
+  //   case '/': header = <Header />
+  //     break;
+  //   case '/login': header = <Header2 />
+  //     break;
+  // }
+  return <>
+  <Routes>
+    <Route path='/' element={<Header/>}/>
+    <Route path='/login' element={<Header2/>}/>
+  </Routes>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
+  </>;
 }
 
 export default App;
---
+++ pet-e-repet/src/pages/login.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../styles/styleLogin.css';
+
+export default function login(){
+    return(
+        <h1>Olá</h1>
+    )
+}
