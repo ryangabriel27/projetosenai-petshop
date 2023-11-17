cfe6a74069386d710da2d63570992ce5fc80d978
vigiaquinze <victorhugopagliuca84@gmail.com>
2023-11-17 17:00:38 -0300

    17/11/23

--- pet-e-repet/src/App.js
+++ pet-e-repet/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Header from './components/header/header.js';
 import HomePage from './components/home/homePage.js';
+import { Slider } from './components/carousel/slider.js';
 
 function App() {
   return (
     <><Header></Header>
       <HomePage></HomePage>
+      <Slider></Slider>
     </>
   );
 }
