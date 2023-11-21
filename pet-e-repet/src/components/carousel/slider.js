a8747c9364d67790fbd6a070b53b551b121049a2
vigiaquinze <victorhugopagliuca84@gmail.com>
2023-11-21 16:54:28 -0300

    21/11 17h

--- pet-e-repet/src/components/carousel/slider.js
+++ pet-e-repet/src/components/carousel/slider.js
@@ -5,9 +5,9 @@ export default function Slider() {
 
   return (
     <NukaCarousel cellAlign="center" slidesToShow={1}>
-      <img src="https://shreveportbossierkids.com/wp-content/uploads/2016/06/shreveport-kids-background.jpg" alt="Slide 1" />
-      <img src="https://shreveportbossierkids.com/wp-content/uploads/2016/06/shreveport-kids-background.jpg" alt="Slide 2" />
-      <img src="https://shreveportbossierkids.com/wp-content/uploads/2016/06/shreveport-kids-background.jpg" alt="Slide 3" />
+      <img src="https://i.ibb.co/zfKzxkc/image.png" alt="Slide 1" />
+      <img src="https://i.ibb.co/zfKzxkc/image.png" alt="Slide 2" />
+      <img src="https://i.ibb.co/zfKzxkc/image.png" alt="Slide 3" />
     </NukaCarousel>
   );
 }
