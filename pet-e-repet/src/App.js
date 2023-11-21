8e5f7fe3a00e74f0898c84f0896a4079a7a746b6
ryangabriel27 <ryanjulio56@gmail.com>
2023-11-21 16:43:13 -0300

    21.11

--- pet-e-repet/src/App.js
+++ pet-e-repet/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import Header from './components/header/header.js';
 import HomePage from './components/home/homePage.js';
-<<<<<<< HEAD
 import Login from './pages/login.js';
 import Header2 from './components/header2/header2.js';
 import { Routes, Route } from 'react-router-dom';
@@ -17,26 +16,15 @@ function App() {
   //     break;
   // }
   return <>
-  <Routes>
-    <Route path='/' element={<Header/>}/>
-    <Route path='/login' element={<Header2/>}/>
-  </Routes>
-      <Routes>
-        <Route path='/' element={<HomePage/>}/>
-        <Route path='/login' element={<Login/>}/>
-      </Routes>
+    <Routes>
+      <Route path='/' element={<Header />} />
+      <Route path='/login' element={<Header2 />} />
+    </Routes>
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/login' element={<Login />} />
+    </Routes>
   </>;
-=======
-import { Slider } from './components/carousel/slider.js';
-
-function App() {
-  return (
-    <><Header></Header>
-      <HomePage></HomePage>
-      <Slider></Slider>
-    </>
-  );
->>>>>>> cfe6a74069386d710da2d63570992ce5fc80d978
 }
 
 export default App;
