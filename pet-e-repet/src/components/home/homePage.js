2ca0fc8b0e4fd8fc000c43f73789109dd56abf38
ryangabriel27 <ryanjulio56@gmail.com>
2023-11-28 13:39:37 -0300

    Pagina home atualizada!

--- pet-e-repet/src/components/home/homePage.js
+++ pet-e-repet/src/components/home/homePage.js
@@ -5,21 +5,24 @@ import './home.css';
 
 
 export default function HomePage() {
-    return (<><nav className="atendimentos">
-        <div className="banho-e-tosa">
-            <p>Banho & Tosa</p>
+    return (<>
+        <nav className="atendimentos">
+            <div className="banho-e-tosa">
+                <p>Banho & Tosa</p>
+            </div>
+            <div className="adestramento">
+                <p>Adestramento</p>
+            </div>
+            <div className="adote">
+                <p>Adote</p>
+            </div>
+            <div className="blog">
+                <p>Blog</p>
+            </div>
+        </nav>
+        <div className="carrosel">
+            <Slider />
         </div>
-        <div className="adestramento">
-            <p>Adestramento</p>
-        </div>
-        <div className="adote">
-            <p>Adote</p>
-        </div>
-        <div className="blog">
-            <p>Blog</p>
-        </div>
-    </nav>
-        <Slider />
         <InfoHome />
     </>
     )
