635a99b8aa4624ca364ff3684b3a1db898ef3da6
vigiaquinze <victorhugopagliuca84@gmail.com>
2023-12-04 14:38:09 -0300

    04/12 14h40

--- pet-e-repet/src/pages/login.js
+++ pet-e-repet/src/pages/login.js
@@ -8,8 +8,8 @@ export default function Login() {
 
 
     return (
-        <div className='main'>
-            <div className="login">
+        <div class="main">
+            <div class="login">
                 <div class="content">
                     <h2>Fazer Login</h2>
                     <p>Use os campos abaixo para entra na sua conta P&R</p>
@@ -17,7 +17,7 @@ export default function Login() {
                         <div class="inputBox">
                             <input type="text" placeholder='Digite seu CPF' required style={{ backgroundImage: `url(${userIcon})`, backgroundRepeat: "no-repeat", backgroundPosition: "20px 18px", padding: "12px 45px 12px 60px" }} />
                         </div>
-                        <div className="inputBox">
+                        <div class="inputBox">
                             <input type="password" placeholder='Digite sua senha' required style={{ backgroundImage: `url(${senhaIcon})`, backgroundRepeat: "no-repeat", backgroundPosition: "20px 18px", padding: "12px 45px 12px 60px" }} />
                         </div>
                         <div class="inputBox buttonEnviar">
@@ -26,7 +26,7 @@ export default function Login() {
                     </form>
                 </div>
             </div>
-            <div className='cadastro'>
+            <div class='cadastro'>
                 <div style={{ width: 500, height: 129, left: 0, top: 0, color: '#090C20', fontSize: 45, fontFamily: 'Fredoka', fontWeight: '900', wordWrap: 'break-word' }}>Criar uma conta é rápido, fácil e gratuito!</div>
                 <div style={{ width: 400, height: 150, left: 0, top: 110, color: '#243387', fontSize: 23, fontFamily: 'Product Sans', fontWeight: '400', wordWrap: 'break-word' }}>Com a sua conta da P&R você tem acesso a Ofertas exclusivas, descontos, pode criar e gerenciar a sua Assinatura P&R, acompanhar os seus pedidos e muito mais!</div>
                 <Link to='/cadastro'><div style={{ width: 401, height: 39, left: 0, top: 267, color: '#CB445B', fontSize: 40, fontFamily: 'Fredoka', fontWeight: '800', wordWrap: 'break-word', textDecoration: 'underline' }}>Cadastre-se</div></Link>
