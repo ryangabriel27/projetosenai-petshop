dc2df98267e24c415fe1b7fc93a9d67f8e961133
vigiaquinze <victorhugopagliuca84@gmail.com>
2023-12-11 14:13:39 -0300

    11/12 14h15

--- pet-e-repet/src/components/produtos/produtos.js
+++ pet-e-repet/src/components/produtos/produtos.js
@@ -1,7 +1,5 @@
 import React from "react";
-import Header from '../header/header';
 import './produtos.css'
-import { Link } from 'react-router-dom';
 
 export default function Produtos() {
     return (
@@ -11,79 +9,7 @@ export default function Produtos() {
             </div>
             <div class="cardsProdutos">
                 <div class="card">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuKMG6ofxY3qEXxZ2h7vGIaBBLU3z0Av1cI1KlDJO7nV-IMY3FSUpgVl7OmiW9ShwNTB4&usqp=CAU"></img>
-                    <h2>Item</h2>
-                    <p>Descrição do item, como o peso, a marca e outras especificações</p>
-                    <button>Comprar</button>
-                </div>
-                <div class="card">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuKMG6ofxY3qEXxZ2h7vGIaBBLU3z0Av1cI1KlDJO7nV-IMY3FSUpgVl7OmiW9ShwNTB4&usqp=CAU"></img>
-                    <h2>Item</h2>
-                    <p>Descrição do item, como o peso, a marca e outras especificações</p>
-                    <button>Comprar</button>
-                </div>
-                <div class="card">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuKMG6ofxY3qEXxZ2h7vGIaBBLU3z0Av1cI1KlDJO7nV-IMY3FSUpgVl7OmiW9ShwNTB4&usqp=CAU"></img>
-                    <h2>Item</h2>
-                    <p>Descrição do item, como o peso, a marca e outras especificações</p>
-                    <button>Comprar</button>
-                </div>
-                <div class="card">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuKMG6ofxY3qEXxZ2h7vGIaBBLU3z0Av1cI1KlDJO7nV-IMY3FSUpgVl7OmiW9ShwNTB4&usqp=CAU"></img>
-                    <h2>Item</h2>
-                    <p>Descrição do item, como o peso, a marca e outras especificações</p>
-                    <button>Comprar</button>
-                </div>
-                <div class="card">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuKMG6ofxY3qEXxZ2h7vGIaBBLU3z0Av1cI1KlDJO7nV-IMY3FSUpgVl7OmiW9ShwNTB4&usqp=CAU"></img>
-                    <h2>Item</h2>
-                    <p>Descrição do item, como o peso, a marca e outras especificações</p>
-                    <button>Comprar</button>
-                </div>
-                <div class="card">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuKMG6ofxY3qEXxZ2h7vGIaBBLU3z0Av1cI1KlDJO7nV-IMY3FSUpgVl7OmiW9ShwNTB4&usqp=CAU"></img>
-                    <h2>Item</h2>
-                    <p>Descrição do item, como o peso, a marca e outras especificações</p>
-                    <button>Comprar</button>
-                </div>
-                <div class="card">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuKMG6ofxY3qEXxZ2h7vGIaBBLU3z0Av1cI1KlDJO7nV-IMY3FSUpgVl7OmiW9ShwNTB4&usqp=CAU"></img>
-                    <h2>Item</h2>
-                    <p>Descrição do item, como o peso, a marca e outras especificações</p>
-                    <button>Comprar</button>
-                </div>
-                <div class="card">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuKMG6ofxY3qEXxZ2h7vGIaBBLU3z0Av1cI1KlDJO7nV-IMY3FSUpgVl7OmiW9ShwNTB4&usqp=CAU"></img>
-                    <h2>Item</h2>
-                    <p>Descrição do item, como o peso, a marca e outras especificações</p>
-                    <button>Comprar</button>
-                </div>
-                <div class="card">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuKMG6ofxY3qEXxZ2h7vGIaBBLU3z0Av1cI1KlDJO7nV-IMY3FSUpgVl7OmiW9ShwNTB4&usqp=CAU"></img>
-                    <h2>Item</h2>
-                    <p>Descrição do item, como o peso, a marca e outras especificações</p>
-                    <button>Comprar</button>
-                </div>
-                <div class="card">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuKMG6ofxY3qEXxZ2h7vGIaBBLU3z0Av1cI1KlDJO7nV-IMY3FSUpgVl7OmiW9ShwNTB4&usqp=CAU"></img>
-                    <h2>Item</h2>
-                    <p>Descrição do item, como o peso, a marca e outras especificações</p>
-                    <button>Comprar</button>
-                </div>
-                <div class="card">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuKMG6ofxY3qEXxZ2h7vGIaBBLU3z0Av1cI1KlDJO7nV-IMY3FSUpgVl7OmiW9ShwNTB4&usqp=CAU"></img>
-                    <h2>Item</h2>
-                    <p>Descrição do item, como o peso, a marca e outras especificações</p>
-                    <button>Comprar</button>
-                </div>
-                <div class="card">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuKMG6ofxY3qEXxZ2h7vGIaBBLU3z0Av1cI1KlDJO7nV-IMY3FSUpgVl7OmiW9ShwNTB4&usqp=CAU"></img>
-                    <h2>Item</h2>
-                    <p>Descrição do item, como o peso, a marca e outras especificações</p>
-                    <button>Comprar</button>
-                </div>
-                <div class="card">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuKMG6ofxY3qEXxZ2h7vGIaBBLU3z0Av1cI1KlDJO7nV-IMY3FSUpgVl7OmiW9ShwNTB4&usqp=CAU"></img>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuKMG6ofxY3qEXxZ2h7vGIaBBLU3z0Av1cI1KlDJO7nV-IMY3FSUpgVl7OmiW9ShwNTB4&usqp=CAU" alt="Imagem do produto"></img>
                     <h2>Item</h2>
                     <p>Descrição do item, como o peso, a marca e outras especificações</p>
                     <button>Comprar</button>
