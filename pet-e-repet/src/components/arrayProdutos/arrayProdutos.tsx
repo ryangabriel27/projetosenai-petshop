46875c9d58d0b671c04b9e1045f65c1d8713c0b4
ryangabriel27 <ryanjulio56@gmail.com>
2023-12-12 14:56:10 -0300

    intervalo

--- pet-e-repet/src/components/arrayProdutos/arrayProdutos.tsx
+++ pet-e-repet/src/components/arrayProdutos/arrayProdutos.tsx
@@ -9,17 +9,17 @@ function ArrayProdutos({ categoria }: { categoria: string }) {
 
     return (
         <div className="cardsProdutos">
-                {data
-                    .filter((item: Props) => categoria ? item.categoria.toLowerCase() === categoria.toLowerCase() : true)
-                    .map((item: Props, index) => (
-                        <div className="card">
-                            <img src={item.foto}></img>
-                            <h2>{item.nome}</h2>
-                            <p>{item.descricao}</p>
-                            <h1>{item.preco}</h1>
-                            <button>Comprar</button>
-                        </div>
-                    ))}
+            {PRODUTOS
+                .filter((item: Props) => categoria ? item.categoria.toLowerCase() === categoria.toLowerCase() : true)
+                .map((item: Props, index) => (
+                    <div className="card">
+                        <img src={item.foto} alt="pics"></img>
+                        <h2>{item.nome}</h2>
+                        <p>{item.descricao}</p>
+                        <h1>R$ {item.preco}</h1>
+                        <button onClick={() => addToCart(item.id)}>Comprar</button>
+                    </div>
+                ))}
         </div>
     )
 }
