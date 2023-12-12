aff46a75b0bba06f113ef610828859c0b675d7d4
vigiaquinze <victorhugopagliuca84@gmail.com>
2023-12-12 14:21:00 -0300

    12/12 14h20

--- pet-e-repet/src/components/home/carousel/slider.js
+++ pet-e-repet/src/components/home/carousel/slider.js
@@ -1,13 +1,16 @@
 import "./slider.css";
 import NukaCarousel from "nuka-carousel";
+import carousel1 from './img/0.png'
+import carousel2 from './img/1.png'
+import carousel3 from './img/2.png'
 
 export default function Slider() {
 
   return (
     <NukaCarousel cellAlign="center" slidesToShow={1} wrapAround autoplay withoutControls>
-      <img src="https://i.ibb.co/TkDrVKG/0.png" alt="Slide 1" />
-      <img src="https://i.ibb.co/6st1fX1/1.png" alt="Slide 2" />
-      <img src="https://i.ibb.co/8PYm8R5/2.png" alt="Slide 3" />
+      <img src={carousel1} alt="Slide 1" />
+      <img src={carousel2} alt="Slide 2" />
+      <img src={carousel3} alt="Slide 3" />
     </NukaCarousel>
   );
 }
