bc70e727832ae9088b2735ccd2a884820fe3d804
ryangabriel27 <ryanjulio56@gmail.com>
2023-12-14 23:29:15 -0300

    commits

--- server/src/server.js
+++ server/src/server.js
@@ -6,18 +6,18 @@ const cors = require('cors');
 const bcrypt = require("bcryptjs");
 const saltRounds = 10;
 
-const db = mysql2.createConnection({
+const db = mysql2.createConnection({ //Passando as informações do banco de dados para criar a conexão
     host: "localhost",
     user: "root",
     password: "root",
     database: "clientes"
 });
 
-db.connect((err) => {
+db.connect((err) => { // Fazendo a conexão
     if (err) {
-        console.log(err)
+        console.log(err) // Caso, de um erro
     } else {
-        console.log('Conectado ao mySql..')
+        console.log('Conectado ao mySql..') // Mostra no console
     }
 })
 
@@ -38,20 +38,20 @@ app.get('/', (req, res) => {
 })
 
 app.post("/cadastrar", async (req, res) => {
-
+    /* Pega os valores digitados nos campos */
     const cadCpf = req.body.cadastroCpf;
     const cadUsuario = req.body.cadastroUsuario;
     const cadSenha = req.body.cadastroSenha;
     const cadCep = req.body.cadastroCEP;
 
-    db.query("SELECT * FROM clientes WHERE cpf = ?", [cadCpf], (err, result) => {
+    db.query("SELECT * FROM clientes WHERE cpf = ?", [cadCpf], (err, result) => { // Verifica se o cpf ja não foi cadastrado
         if (err) {
             res.send(err);
         }
 
         console.log(result)
 
-        if (result.length == 0) {
+        if (result.length == 0) { // Se não, cria um novo usuario
             db.query(
                 "INSERT INTO clientes(cpf, nome, senha, cep) VALUE (?,?,?,?)",
                 [cadCpf, cadUsuario, cadSenha, cadCep],
@@ -72,12 +72,12 @@ app.post("/logar", async (req, res) => {
     const logCpf = req.body.loginCpf;
     const logSenha = req.body.loginSenha;
 
-    db.query("SELECT * FROM clientes WHERE cpf = ? and senha = ?", [logCpf, logSenha], (err, result) => {
+    db.query("SELECT * FROM clientes WHERE cpf = ? and senha = ?", [logCpf, logSenha], (err, result) => { // Procura no banco de dados um cpf e uma senha que sejam iguais a os que foram digitados pelo usuario
         if (err) {
             res.send(err);
         }
         console.log(result);
-        if (result.length > 0) {
+        if (result.length > 0) { // Se achar, usuario logado!
             res.send({ msg: "Usuário logado" })
         } else {
             res.send({ msg: "Usuário não registrado!" });
